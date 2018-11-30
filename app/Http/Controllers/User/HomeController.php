1af4a6b4972e619987e398c2fe38f44b50f1d757
fahriansyah <fachryansyah123@gmail.com>
2018-11-30 21:45:42 +0700

    tambahin tampilan home web user

---
+++ app/Http/Controllers/User/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    //ini untuk tampilan user > home
+    public function index()
+    {
+    	return view('layouts.user.home');
+    }
+}
