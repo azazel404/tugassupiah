be4539c9271e88b1290814a1fdc505845abf0e23
fahriansyah <fachryansyah123@gmail.com>
2018-11-30 23:55:08 +0700

    nambah page home

--- resources/views/layouts/user/app.blade.php
+++ resources/views/layouts/user/app.blade.php
@@ -1,12 +1,96 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Bank Bpr</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
 </head>
 <body>
+	<div class="container-nav">
+		<nav class="mt-4 navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">
+				<img src="{{ asset('img/logo_bprdn.png') }}" class="img-fluid">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item dropdown active">
+						<a class="nav-link dropdown-toggle" href="#" id="dropdownTentang" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Tentang Kami
+						</a>
+						<div class="dropdown-menu" aria-labelledby="dropdownTentang">
+							<a class="dropdown-item" href="#">Profile Perusahaan</a>
+							<a class="dropdown-item" href="#">Laporan</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Something else here</a>
+						</div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">News & Promo</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Produk
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Action</a>
+							<a class="dropdown-item" href="#">Another action</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Something else here</a>
+						</div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Hubungi kami</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
 	@yield('content')
+	<footer class="main-footer mt-9">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 mt-5">
+					<div class="logo">
+						<h4 class="">BPR Agra Dhana</h4>
+					</div>
+					<div class="contact-details">
+						<p>Alamat: Komp. Nagoya City Centre blok E No.12, Lubuk Baja,
+						Kota Batam, Kep. Riau</p>
+						<p>Telp: (0778) 429199</p>
+						<p>Email: <a href="mailto:ptbpragradhana@yahoo.co.id">ptbpragradhana@yahoo.co.id</a></p>
+
+						<ul class="social-menu">
+							<li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
+
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-6 mt-5">
+					<div class="menus d-flex justify-content-end">
+						<ul class="list-unstyled">
+							<li> <a href="http://bprad.com/laporan-publikasi">Laporan Publikasi</a></li>
+							<li> <a href="http://bprad.com/laporan-kelola">Tata Kelola</a></li>
+							<li> <a href="http://bprad.com/profile-perusahaan">Profile Perusahaan</a></li>
+							<li> <a href="http://bprad.com/karir">News &amp; Promo</a></li>
+						</ul>
+						<ul class="list-unstyled">
+							<li> <a href="http://bprad.com/product/tabungan">Tabungan</a></li>
+							<li> <a href="http://bprad.com/product/deposito">Deposito</a></li>
+							<li> <a href="http://bprad.com/product/kredit">Kredit</a></li>
+							<li> <a href="http://bprad.com/simulasi">simulasi</a></li>
+						</ul>
+					</div>
+				</div>
+      	</div>
+      </footer>
 	<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
---
+++ resources/views/layouts/user/home.blade.php
@@ -0,0 +1,133 @@
+@extends('layouts.user.app')
+@section('content')
+
+<div class="container mt-5">
+	<div class="row mb-5">
+		<div class="col-md-12 col-12">
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img class="d-block w-100" src="{{ asset('img/Banner_ATS.jpg') }}" alt="First slide">
+					</div>
+					<div class="carousel-item">
+						<img class="d-block w-100" src="{{ asset('img/deposito_dana_hoki.jpg') }}" alt="Second slide">
+					</div>
+					<div class="carousel-item">
+						<img class="d-block w-100" src="{{ asset('img/simpel_danus.jpg') }}" alt="Third slide">
+					</div>
+				</div>
+				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="row mt-7">
+	 	<div class="col-md-12 col-12">
+	 		<div class="text-center">
+	 			<h2 class="font-weight-light">Selamat Datang di Website BPR Agra Dhana, <br> Kami siap melayani kebutuhan finansial Anda.</h2>
+	 		</div>
+	 	</div>
+	</div>
+
+	<div class="row mt-5">
+		<div class="col-md-4 col-12">
+			<div class="text-center p-4">
+				<img src="{{ asset('img/icon/kredit.png') }}" class="img-icon">
+				<h4 class="mt-3 font-weight-bold">Kredit</h4>
+				<p>Kami menghadirkan Produk Jasa Kredit dan Pembiayaan untuk segala kebutuhan anda di BPR Agra Dhana.</p>
+			</div>
+		</div>
+		<div class="col-md-4 col-12">
+			<div class="text-center p-4">
+				<img src="{{ asset('img/icon/deposit.png') }}" class="img-icon">
+				<h4 class="mt-3 font-weight-bold">Deposito</h4>
+				<p>Produk jasa Deposito, memberikan keuntungan aman karena Simpanan anda di BPR Pondasi.</p>
+			</div>
+		</div>
+		<div class="col-md-4 col-12">
+			<div class="text-center p-4">
+				<img src="{{ asset('img/icon/tabungan.png') }}" class="img-icon">
+				<h4 class="mt-3 font-weight-bold">Tabungan</h4>
+				<p>Kami juga menghadirkan berbagai variasi Tabungan dengan bunga yang sangat menguntungkan.</p>
+			</div>
+		</div>
+	</div>
+
+
+	<h2 class="mt-7 font-weight-light">News & Promo</h2>
+	<div class="row mt-3">
+		<div class="col-md-4 col-12">
+			<div class="card">
+				<img class="card-img-top" src="{{ asset('img/deposito_dana_hoki.jpg') }}" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-12">
+			<div class="card">
+				<img class="card-img-top" src="{{ asset('img/simpel_danus.jpg') }}" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-12">
+			<div class="card">
+				<img class="card-img-top" src="{{ asset('img/Banner_ATS.jpg') }}" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row mt-7">
+		<div class="col-md-12 col-12">
+			<h6>BPR PONDASI terdaftar dan diawasi oleh:</h6>
+		</div>
+	</div>
+
+	<div class="row mt-2">
+		<div class="col-md-3 col-5 brand-responsive">
+			<a href="https://www.ojk.go.id/id/Default.aspx">
+				<img class="img-fluid d-block mx-auto" src="{{ asset('img/OJK-logo-baru.jpg') }}" alt="">
+			</a>
+		</div>
+		<div class="col-md-3 col-5 brand-responsive">
+			<a href="http://www.lps.go.id/">
+				<img class="img-fluid d-block mx-auto" src="{{ asset('img/LPS-logo-baru.png') }}" alt="">
+			</a>
+		</div>
+		<div class="col-md-3 col-5 brand-responsive">
+			<a href="https://www.bi.go.id/id/Default.aspx">
+				<img class="img-fluid d-block mx-auto" src="{{ asset('img/lobo-bi-baru.png') }}" alt="">
+			</a>
+		</div>
+		<div class="col-md-3 col-5 brand-responsive">
+			<a href="http://ayokebank.com/">
+				<img class="img-fluid d-block mx-auto" src="{{ asset('img/logo-ayo-ke-bank.png') }}" alt="">
+			</a>
+		</div>
+	</div>
+</div>
+
+@endsection
