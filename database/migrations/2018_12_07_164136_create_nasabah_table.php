d97336b820474cbf380de1f885287802d67f6a54
fahriansyah <fachryansyah123@gmail.com>
2018-12-07 23:44:25 +0700

    tambah migrasi nasabah

---
+++ database/migrations/2018_12_07_164136_create_nasabah_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNasabahTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nasabahs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code_customer');
+            $table->string('rekening');
+            $table->string('name');
+            $table->string('email', 120)->unique();
+            $table->text('address')->nullable();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nasabahs');
+    }
+}
