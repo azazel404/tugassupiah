93bda63ba4d15c9752829c8cde036646ab2b7b4a
fahriansyah <fachryansyah123@gmail.com>
2018-12-09 01:14:58 +0700

    nambahin 2 fungsi login admin

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    //
+	//fungsi untuk menampilkan form login
+	public function FunctionName()
+	{
+		return view('layouts.auth.login');
+	}
+
+    //fungsi untuk login admin
+    public function login(LoginRequest $req)
+    {
+    	$credential = ["email" => $req->email, "password" => $req->password];
+    	if (Auth::guard('admin')->attempt($credential)) {
+    		return "berhasil login admin";
+    	}
+    	return "email atau password salah";
+
+    }
 }
