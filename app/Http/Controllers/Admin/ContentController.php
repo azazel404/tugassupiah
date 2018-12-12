54988283f699738bc0c6879ab498a7d152a212c2
fahriansyah <fachryansyah123@gmail.com>
2018-12-13 00:19:18 +0700

    create content

--- app/Http/Controllers/Admin/ContentController.php
+++ app/Http/Controllers/Admin/ContentController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Content;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ContentRequest;
 
 class ContentController extends Controller
 {
@@ -11,5 +13,15 @@ class ContentController extends Controller
     public function addContent()
     {
         return view('layouts.admin.content.add');
+    }
+
+    public function createContent(ContentRequest $req)
+    {
+        $content = new Content;
+        $cover = $req->cover->store('public/cover');
+        $content->title = $req->title;
+        $content->cover = basename($cover);
+        $content->content = $req->content;
+        $content->save();
     }
 }
