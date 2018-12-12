8da335527da4b2c11cba0fa0c1edcc95982ffe6a
fahriansyah <fachryansyah123@gmail.com>
2018-12-12 23:07:32 +0700

    nambahin fitur content

--- app/Content.php
+++ app/Content.php
@@ -4,8 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CategoryItem extends Model
+class Content extends Model
 {
     //
-    protected $table = "category_items";
+    protected $table = "contents";
 }
