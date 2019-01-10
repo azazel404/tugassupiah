48e3652df913c88824cfc51be55f214757a20830
fahriansyah <fachryansyah123@gmail.com>
2019-01-10 13:35:14 +0700

    ganti nama bpr pundi jadi bpr pondasi

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Admin | Bpr Pundi</title>
+	<title>Admin | Bpr Pondasi</title>
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="{{ asset('plugins/font-awesome/css/font-awesome.min.css') }}">
 	<!-- Ionicons -->
@@ -61,7 +61,7 @@
 		<a href="index3.html" class="brand-link">
 			<img src="{{ asset('img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
 			style="opacity: .8">
-			<span class="brand-text font-weight-light">AdminLTE 3</span>
+			<span class="brand-text font-weight-light">BPR Pondasi</span>
 		</a>
 
 		<!-- Sidebar -->
@@ -72,7 +72,7 @@
 	          <img src="{{ asset('img/avatar.png') }}" class="img-circle elevation-2" alt="User Image">
 	        </div>
 	        <div class="info">
-	          <a href="#" class="d-block">Alexander Pierce</a>
+	          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
 	        </div>
 	      </div>
 
