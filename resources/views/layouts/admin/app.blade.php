9b9fc3b975f066b4e7395a5a7bffea4e8a09e433
Fahriansyah <fachryansyah123@gmail.com>
2019-02-11 09:24:59 +0700

    fix logo dipanel admin

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -59,7 +59,7 @@
 	<aside class="main-sidebar sidebar-dark-primary elevation-4"class="main-sidebar sidebar-dark-primary elevation-4">
 		<!-- Brand Logo -->
 		<a href="index3.html" class="brand-link">
-			<img src="{{ asset('img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+			<img src="{{ asset('img/logo.png') }}" alt="AdminLTE Logo" class="brand-image"
 			style="opacity: .8">
 			<span class="brand-text font-weight-light">BPR Pondasi</span>
 		</a>
