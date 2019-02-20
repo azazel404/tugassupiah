2dbaf3d4cfcfdd1d9b372476492b74722055f0dd
Fahriansyah <fachryansyah123@gmail.com>
2019-02-20 14:59:27 +0700

    benerin lokasi map yg salah

--- resources/views/layouts/user/contact.blade.php
+++ resources/views/layouts/user/contact.blade.php
@@ -5,7 +5,7 @@
 	<div class="row mt-5">
 		<div class="col-md-8 col-12">
 			<h1 class="font-weight-bold">Hubungi Kami</h1>
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.851669513505!2d106.84972711379898!3d-6.413098995359914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ebb04f6e16fd%3A0xaf59f873ded532c5!2sJl.+H.+Dimun+III%2C+Sukamaju%2C+Cilodong%2C+Kota+Depok%2C+Jawa+Barat+16415!5e0!3m2!1sen!2sid!4v1543848231934" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.6110759900334!2d106.98659221410954!3d-6.182777145523914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698bec197c772d%3A0x5c8eee1c44eba34!2sPT.+BPR+Pondasi+Niaga+Perdana!5e0!3m2!1sen!2sid!4v1550649491060" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 			<h3 class="mt-4">Marketing</h3>
 			<ul class="list-unstyled">
 				@foreach($marketings as $marketing)
