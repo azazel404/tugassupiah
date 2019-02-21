a2ffd2e6420df69002888d261bd78d55fb49dec1
Fahriansyah <fachryansyah123@gmail.com>
2019-02-21 15:41:34 +0700

    update

--- resources/views/layouts/admin/pengajuanDeposito/list.blade.php
+++ resources/views/layouts/admin/pengajuanDeposito/list.blade.php
@@ -74,7 +74,7 @@
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLabel">Tambah pengajuan tabungan</h5>
+				<h5 class="modal-title" id="exampleModalLabel">Tambah pengajuan deposito</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
