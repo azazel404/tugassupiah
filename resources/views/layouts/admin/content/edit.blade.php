7a5fa4d7ddee0715948bfee399fa5285dee77c76
Fahriansyah <fachryansyah123@gmail.com>
2019-02-21 15:01:38 +0700

    update edit content

--- resources/views/layouts/admin/content/edit.blade.php
+++ resources/views/layouts/admin/content/edit.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.admin.app')
+@section('stylesheet')
+<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+@endsection
 @section('content')
 <!-- Content Header (Page header) -->
 <div class="content-header">
@@ -67,7 +70,62 @@
 							</div>
                             <div class="form-group">
                                 <label>Konten</label>
-                                <textarea id="content" name="content">{{ $content->content }}</textarea>
+                                <div id="progress" class="progress">
+									<div id="progress-bar" class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+										<b id="progress-bar-value">25%</b>										
+									</div>
+								</div>
+                                <textarea id="content" name="content" style="display:none;">{{ $content->content }}</textarea>
+                                <div id="standalone-container">
+                                	<div id="toolbar-container">
+                                		<span class="ql-formats">
+                                			<select class="ql-font"></select>
+                                			<select class="ql-size"></select>
+                                		</span>
+                                		<span class="ql-formats">
+                                			<button class="ql-bold"></button>
+                                			<button class="ql-italic"></button>
+                                			<button class="ql-underline"></button>
+                                			<button class="ql-strike"></button>
+                                		</span>
+                                		<span class="ql-formats">
+                                			<select class="ql-color"></select>
+                                			<select class="ql-background"></select>
+                                		</span>
+                                		<span class="ql-formats">
+                                			<button class="ql-script" value="sub"></button>
+                                			<button class="ql-script" value="super"></button>
+                                		</span>
+                                		<span class="ql-formats">
+                                			<button class="ql-header" value="1"></button>
+                                			<button class="ql-header" value="2"></button>
+                                			<button class="ql-blockquote"></button>
+                                			<button class="ql-code-block"></button>
+                                		</span>
+                                		<span class="ql-formats">
+                                			<button class="ql-list" value="ordered"></button>
+                                			<button class="ql-list" value="bullet"></button>
+                                			<button class="ql-indent" value="-1"></button>
+                                			<button class="ql-indent" value="+1"></button>
+                                		</span>
+                                		<span class="ql-formats">
+                                			<button class="ql-direction" value="rtl"></button>
+                                			<select class="ql-align"></select>
+                                		</span>
+                                		<span class="ql-formats">
+                                			<button class="ql-link"></button>
+                                			<button class="ql-image"></button>
+                                			<button class="ql-video"></button>
+                                			<button class="ql-formula"></button>
+                                		</span>
+                                		<span class="ql-formats">
+                                			<button class="ql-clean"></button>
+                                		</span>
+                                	</div>
+                                	<div id="editor">
+                                		{!! $content->content !!}
+                                	</div>
+                                </div>
                             </div>
 						</form>
 					</div>
@@ -80,19 +138,12 @@
 
 @section('script')
 <!-- include summernote css/js -->
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/quill-image-upload@0.1.3/image-upload.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/quill-image-resize-module@3.0.0/image-resize.min.js"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-bs4.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-bs4.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('#content').summernote({
-            height: 300,
-            callbacks:{
-                onImageUpload: function(files, editor, welEditable){
-                    uploadImage(files[0], editor, welEditable);
-                }
-            }
-        });
-    });
 
     function uploadImage(file, editor, welEditable) {
         data = new FormData();
@@ -129,5 +180,45 @@
 			}
 		})
 	}
+
+	let options = {
+		modules: {
+			toolbar: '#toolbar-container',
+			imageUpload: {
+	            url: '/admin/upload-image',
+	            method: 'POST',
+	            name: 'file',
+	            withCredentials: false,
+	            headers: {},
+	            beforeSend: function() {
+	            	progress.show()
+	            	progressBarValue.text('0%')
+	            },
+	            uploadProgress: function(event, position, total, percentComplete) {
+	            	progressBar.attr('aria-valuenow', percentComplete)
+	            },
+	            callbackOK: (serverResponse, next) => {
+	                next(serverResponse.responseText);
+	            },
+	            callbackKO: serverError => {
+	                alert(serverError);
+	            }
+	        },
+	        imageResize: {
+	        	modules: [ 'Resize', 'DisplaySize', 'Toolbar' ]
+	        }
+		},
+		// imageHandler: imageHandler,
+		theme: 'snow'
+	}
+
+	var quill = new Quill('#editor', options);
+	var imageResize = quill.getModule('imageResize');
+
+	quill.on('text-change', function() {
+	    let html = $('.ql-editor').html();
+	    console.log(html);
+	    $('#content').val(html);
+	});
 </script>
 @endsection
